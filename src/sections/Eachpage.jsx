2113de40afb3f4564021e773692123a3fcf53f01
Farouk-ayo <mustaphafarouk41@gmail.com>
2023-09-16 19:21:13 +0100

    youtube

--- src/sections/Eachpage.jsx
+++ src/sections/Eachpage.jsx
@@ -1,6 +1,5 @@
 // import React from "react";
 import classes from "./Eachpage.module.scss";
-import { HiMiniPlay } from "react-icons/hi2";
 import { BsListUl } from "react-icons/bs";
 import { AiFillStar } from "react-icons/ai";
 import ticket from "../assets/tickets.svg";
@@ -14,16 +13,27 @@ const Eachpage = () => {
   const movie_id = params.id;
   const [details, setDetails] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [video, setVideo] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await apiFetch(`${movie_id}`);
+      const dataVideo = await apiFetch(`${movie_id}/videos?language=en-US`);
       setDetails(data);
+      setVideo(dataVideo.results[0]);
       setIsLoading(false);
     };
 
     fetchData();
   }, [movie_id]);
+
+  const getUtc = () => {
+    const date = new Date(details.release_date).toUTCString();
+    const newDate = date.split(" ").slice(0, 4).join(" ");
+    return newDate;
+  };
+
+  console.log(video);
 
   return (
     <>
@@ -32,23 +42,21 @@ const Eachpage = () => {
       ) : (
         <main className={classes.eachpage}>
           <section className={classes.movie}>
-            <img
-              src={`https://image.tmdb.org/t/p/original/${details.poster_path}`}
-              alt=""
-            />
-            <HiMiniPlay className={classes.play} color="white" size={40} />
-            <a href="">Watch Trailer</a>
+            <iframe
+              width="100%"
+              height="500"
+              className={classes.trailer}
+              src={`https://www.youtube.com/embed/${video.key}`}
+            ></iframe>
           </section>
           <section className={classes.details}>
             <div className={classes.left}>
               <h3>
                 <span data-testid="movie-title">{details.title}</span>
                 <span>•</span>
-                <span data-testid="movie-release-date>2022">
-                  {details.release_date}
-                </span>
+                <span data-testid="movie-release-date">{getUtc()}</span>
                 <span>•</span>
-                <span data-testid="movie-runtime">{details.runtime}</span>
+                <span data-testid="movie-runtime">{`${details.runtime} minutes`}</span>
               </h3>
               <div>
                 <span>Action</span>
